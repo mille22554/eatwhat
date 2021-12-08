3bb2ec0a988235d56c21f966680064c3fc336607
9a713032 <9a713032@gmail.com>
2021-12-08 10:14:39 +0800

    1.4

---
+++ app/src/main/java/com/moro/eatwhat/MainActivity3.java
@@ -0,0 +1,37 @@
+package com.moro.eatwhat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+    public void newshop(View view){
+        gv gv=(gv) getApplicationContext();
+        EditText et=findViewById(R.id.editTextTextPersonName2);
+        gv.list.add(String.valueOf(et.getText()));
+        gv.shopinfo.add("無");
+        Toast.makeText(this, "新增成功", Toast.LENGTH_LONG).show();
+    }
+    public void Return(View view){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity3.this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/moro/eatwhat/gv.java
@@ -0,0 +1,10 @@
+package com.moro.eatwhat;
+
+import android.app.Application;
+import java.util.ArrayList;
+
+public class gv extends Application {
+    public static String shopname="";
+    public ArrayList<String> list=new ArrayList<String>();
+    public ArrayList<String> shopinfo=new ArrayList<String>();
+}
